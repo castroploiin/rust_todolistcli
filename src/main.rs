39a666ca565b4e48463083862592442f15496612
castroploiin <fieryfire1005@gmail.com>
2023-12-13 01:03:32 -0500

    Refactor: removed repetition

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use csv::{Reader, StringRecord, WriterBuilder};
 use std::fs;
 use std::io::{self, LineWriter, Write};
 use std::path::Path;
-use csv::{Reader, StringRecord, WriterBuilder};
 
 fn main() -> Result<(), io::Error> {
     loop {
@@ -11,8 +11,8 @@ fn main() -> Result<(), io::Error> {
 }
 
 #[derive(serde::Serialize)]
-struct Task<'a> { 
-    title: &'a str, 
+struct Task<'a> {
+    title: &'a str,
 }
 
 fn parse_input(arg: String) -> Result<(), io::Error> {
@@ -23,11 +23,11 @@ fn parse_input(arg: String) -> Result<(), io::Error> {
         "restore" => restore_tasks()?,
         "list" => list_tasks()?,
         "quit" => std::process::exit(69),
-        _ => return Ok(())
+        _ => return Ok(()),
     }
     Ok(())
 }
- 
+
 fn get_input(with_guide: &str) -> Result<String, io::Error> {
     println!("{}", with_guide);
     let mut input = String::new();
@@ -43,9 +43,8 @@ fn add_task() -> Result<(), io::Error> {
     let file_content = fs::read_to_string(Path::new("src/todos.csv"))?;
     let file = open_file()?;
     let mut writer = LineWriter::new(file);
-    
-    writer.write_all((file_content + &task).as_bytes())?;
 
+    writer.write_all((file_content + &task).as_bytes())?;
     Ok(())
 }
 
@@ -97,30 +96,35 @@ fn bufferize() -> Result<(), io::Error> {
 }
 
 fn copy_without(task_name: Option<&String>) -> Result<(), io::Error> {
-    match task_name {
-        Some(task) => {
-            let mut buffer_file = Reader::from_path(Path::new("src/buffer.csv"))?;
-            let mut todo_file = WriterBuilder::new().from_path(Path::new("src/todos.csv"))?;
-            
-            for line in buffer_file.records() {
-                let unwrap_line = line?;
+    let mut buffer_file = Reader::from_path(Path::new("src/buffer.csv"))?;
+    let mut todo_file = WriterBuilder::new().from_path(Path::new("src/todos.csv"))?;
+
+    for line in buffer_file.records() {
+        let unwrap_line = line?;
+        match task_name {
+            Some(task) => {
                 let true_instance = StringRecord::from(vec![format!("{}", task.trim().to_owned())]);
-                let false_instance = StringRecord::from(vec![format!("{}", task.trim().to_owned())]);
-        
+                let false_instance =
+                StringRecord::from(vec![format!("{}", task.trim().to_owned())]);
+
                 if &unwrap_line != &true_instance && &unwrap_line != &false_instance {
-                    let task: Task = Task { title: &unwrap_line[0] };
+                    let task: Task = Task {
+                        title: &unwrap_line[0],
+                    };
                     todo_file.serialize(task)?;
                 }
-            }
-        },
-        None => {
-            let mut buffer_file = Reader::from_path(Path::new("src/buffer.csv"))?;
-            let mut todo_file = WriterBuilder::new().from_path(Path::new("src/todos.csv"))?;
-            
-            for line in buffer_file.records() {
-                let unwrap_line = line?;   
-                let task: Task = Task { title: &unwrap_line[0] };
-                todo_file.serialize(task)?;
+            },
+            None =>  {
+                let mut buffer_file = Reader::from_path(Path::new("src/buffer.csv"))?;
+                let mut todo_file = WriterBuilder::new().from_path(Path::new("src/todos.csv"))?;
+
+                for line in buffer_file.records() {
+                    let unwrap_line = line?;
+                    let task: Task = Task {
+                        title: &unwrap_line[0],
+                    };
+                    todo_file.serialize(task)?;
+                }
             }
         }
     }
@@ -130,15 +134,5 @@ fn copy_without(task_name: Option<&String>) -> Result<(), io::Error> {
 
 fn open_file() -> Result<fs::File, std::io::Error> {
     let file_path = Path::new("src/todos.csv");
-    match file_path.exists() {
-        true => { 
-            let file = fs::File::create(file_path);
-            return file
-        },
-        false => {
-            let file = fs::File::create(file_path);
-
-            return file
-        }
-    }
+    fs::File::create(file_path)
 }
